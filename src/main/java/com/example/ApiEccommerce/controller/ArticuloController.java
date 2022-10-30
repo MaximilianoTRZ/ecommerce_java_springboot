173ee89e24caf5f50e58a8a722cfaf4647061854
Ivan <lealivan009@gmail.com>
2022-10-30 17:53:43 -0300

    ecommerce v1 30-10-22

--- src/main/java/com/example/ApiEccommerce/controller/ArticuloController.java
+++ src/main/java/com/example/ApiEccommerce/controller/ArticuloController.java
@@ -5,26 +5,74 @@ import com.example.ApiEccommerce.entities.Articulo;
 import com.example.ApiEccommerce.services.ArticuloServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
 @CrossOrigin(origins = "*")  //en @Controller esto no es necesario?
 @RequestMapping(path = "api/v1/articulos")  //en @Controller esto no es necesario?
-public class ArticuloController extends BaseControllerImpl<Articulo, ArticuloServiceImpl>{
+public class ArticuloController extends BaseControllerImpl<Articulo, ArticuloServiceImpl> {
+
+    @GetMapping("/carrito")
+    public String carrito(Model model) {
+        try {
+            return "views/carrito";
+        } catch (Exception e) {
+            String mensaje = "hubo un error";
+            model.addAttribute("mensajeError", mensaje);
+            return "error";
+        }
+
+    }
+
+    @GetMapping("/signup")
+    public String signup(Model model) {
+        try {
+            return "views/Signup";
+        } catch (Exception e) {
+            String mensaje = "hubo un error";
+            model.addAttribute("mensajeError", mensaje);
+            return "error";
+        }
+
+    }
+
+    @GetMapping("/signin")
+    public String signin(Model model) {
+        try {
+            return "views/Signin";
+        } catch (Exception e) {
+            String mensaje = "hubo un error";
+            model.addAttribute("mensajeError", mensaje);
+            return "error";
+        }
+
+    }
+
+    @GetMapping("/detalle/{id}")
+    public String detalleArticulo(Model model, @PathVariable("id") long id) {
+        try {
+            Articulo articulo = servicio.findById(id);
+            model.addAttribute("detalleArticulo", articulo);
+            return "views/detalleProducto";
+        } catch (Exception e) {
+            String mensaje = "hubo un error";
+            model.addAttribute("mensajeError", mensaje);
+            return "error";
+        }
+
+    }
 
     @GetMapping("/lista")
     public String ListaArticulos(Model model) throws Exception { //EL NOMBRE DEL METODO ES SOLO REPRESENTATIVO, NO SE UTILIZA
         try {
-            List<Articulo> nombreVariable=servicio.findAll();
+            List<Articulo> nombreVariable = servicio.findAll();
             model.addAttribute("nombreVariable", nombreVariable);
             return "views/productos"; //ACA TIENE QUE IR EL NOMBRE DE LA PLANTILLA DONDE SE QUIERE USAR
-        }catch (Exception e){
-            model.addAttribute("error",e);
+        } catch (Exception e) {
+            String mensaje = "hubo un error";
+            model.addAttribute("mensajeError", mensaje);
             return "error";
         }
     }
@@ -32,11 +80,13 @@ public class ArticuloController extends BaseControllerImpl<Articulo, ArticuloSer
     @GetMapping("/inicio")
     public String inicio(Model model) throws Exception { //EL NOMBRE DEL METODO ES SOLO REPRESENTATIVO, NO SE UTILIZA
         try {
-            String usuario= "Ivan";
+            String usuario = "Master";
             model.addAttribute("nombreVariable", usuario);
             return "views/inicio"; //ACA TIENE QUE IR EL NOMBRE DE LA PLANTILLA DONDE SE QUIERE USAR
-        }catch (Exception e){
-            return "views/error";
+        } catch (Exception e) {
+            String mensaje = "hubo un error";
+            model.addAttribute("mensajeError", mensaje);
+            return "error";
         }
     }
 }
