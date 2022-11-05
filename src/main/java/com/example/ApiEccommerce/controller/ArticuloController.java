900baa5bb927b8feca46133da19188ee632e5e4b
Ivan <lealivan009@gmail.com>
2022-11-05 14:25:16 -0300

    ecommerce v1 5-11-22 funcionando todo lo q hay

--- src/main/java/com/example/ApiEccommerce/controller/ArticuloController.java
+++ src/main/java/com/example/ApiEccommerce/controller/ArticuloController.java
@@ -4,6 +4,7 @@ package com.example.ApiEccommerce.controller;
 import com.example.ApiEccommerce.entities.Articulo;
 import com.example.ApiEccommerce.services.ArticuloServiceImpl;
 import com.example.ApiEccommerce.services.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,7 @@ import java.util.List;
 @RequestMapping(path = "api/v1/articulos")  //en @Controller esto no es necesario?
 public class ArticuloController extends BaseControllerImpl<Articulo, ArticuloServiceImpl> {
 
+    @Autowired
     CategoriaService categoriaService;
     @GetMapping("/busqueda")
     public String busqueda(Model model, @RequestParam(value="query", required = false) String q) {
@@ -96,16 +98,19 @@ public class ArticuloController extends BaseControllerImpl<Articulo, ArticuloSer
     @PostMapping("/guardarArticulo")
     public String guardarArticulo(@ModelAttribute("nuevoArticulo") Articulo articulo) throws Exception {
         servicio.save(articulo);
-        return "redirect:/inicio";
+        return "redirect:/api/v1/articulos/crud";
     }
 
 
     //En este metodo lo que hago es crear el objeto, el cual va a ser cargado con los datos que se completan en el formulario
+    //Envio las categorias porque el articulo tiene asociada una categoria
     @GetMapping("/nuevo")
     public String crearNuevoProducto(Model model) {
         try {
             Articulo nuevoArticulo= new Articulo();
             model.addAttribute("nuevoArticulo", nuevoArticulo);
+            model.addAttribute("categorias", categoriaService.findAll());
+
             return "views/formulario";
         } catch (Exception e) {
             String mensaje = "hubo un error";
